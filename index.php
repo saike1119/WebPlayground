e211ed004a96da036060f3095274136ea24e3370
saike1119 <saike1119@gmail.com>
2018-02-07 10:31:56 +0900

    index.php修正

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>WebPlayground</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<h1>ようこそ！WebPlaygroundへ！</h1>
+<p>ここでは様々なWebに関する技術を楽しめます</p>
+</body>
+</html>
