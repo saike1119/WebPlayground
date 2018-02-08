d666abad4e44b5127cbaa11360df2852603ab61a
saike1119 <saike1119@gmail.com>
2018-02-08 11:39:31 +0900

    walkform.php追加

---
+++ walkform.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>form</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<form action="walk_calc.php" method="post">
+    名前（未入力もok）: <input title="名前を入力" type="text" name="name"/><br>
+    スタート地点: <input title="スタート地点を入力" type="text" name="start"/><br>
+    ゴール地点: <input title="ゴール地点を入力" type="text" name="goal"/><br>
+    移動幅: <input title="移動幅を入力" type="text" name="move"/><br>
+    <input type="submit"/>
+</form>
+</body>
+</html>
