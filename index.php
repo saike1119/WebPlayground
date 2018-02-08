8bc3cc01ec16aa0d996b6444da1a7f44630775b0
saike1119 <saike1119@gmail.com>
2018-02-08 11:18:46 +0900

    h3,ulli追加

--- index.php
+++ index.php
@@ -9,7 +9,10 @@
 <h1>ようこそ！WebPlaygroundへ！</h1>
 <div class="textBox"></div>
 <p>ここでは様々なWebに関する技術を楽しめます</p>
-<a href="#">距離計算</a><br>
-<a href="#">メンバーシャッフル</a>
+<h3>PlayGround</h3>
+<ul type="disc">
+    <li><a href="#">距離計算</a><br></li>
+    <li><a href="#">メンバーシャッフル</a></li>
+</ul>
 </body>
 </html>
