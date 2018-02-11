1266c0e740e1f209662c7204f8d90917aa3f2f69
saike1119 <saike1119@gmail.com>
2018-02-11 18:05:01 +0900

    お野菜管理フォーム作成

---
+++ vegetable_form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>form</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<h1>お野菜管理</h1>
+</form>
+</body>
+</html>
