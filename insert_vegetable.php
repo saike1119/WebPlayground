42a4219999fae525f79e01884d2b8f6af10d0c4a
saike1119 <saike1119@gmail.com>
2018-02-13 13:56:43 +0900

    html追加

--- insert_vegetable.php
+++ insert_vegetable.php
@@ -1,8 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>form</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
 <?php
 
 if(!empty($_POST['vegetable'])) {
     $vegetable = $_POST['vegetable'];
-    var_dump($vegetable);
 }else{
-    echo '値を入力してください';
+    echo '値を入力してください！';
 }
+
+?>
+<br>
+<div class="btn">戻る</div>
+</body>
+</html>
