733499b62660572ff8929ca69605bde807069734
saike1119 <saike1119@gmail.com>
2018-02-24 16:28:11 +0900

    phpの処理位置変更

--- vegetable_form.php
+++ vegetable_form.php
@@ -1,19 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>form</title>
-    <link rel="stylesheet" type="text/css" href="./css/style.css">
-</head>
-<body>
-<h1>お野菜管理</h1>
-<form action="insert_vegetable.php" method="post">
-    野菜名: <input title="名前を入力" type="text" name="name"/><br>
-    価格: <input title="価格を入力" type="text" name="price"/><br>
-    概要: <input title="概要を入力" type="text" name="description"/><br>
-    <input type="submit"/>
-</form>
 <?php
 include_once('db_def.php');
 
@@ -32,6 +16,22 @@ $res = $mysqli->query($sql);
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>form</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<h1>お野菜管理</h1>
+<form action="insert_vegetable.php" method="post">
+    野菜名: <input title="名前を入力" type="text" name="name"/><br>
+    価格: <input title="価格を入力" type="text" name="price"/><br>
+    概要: <input title="概要を入力" type="text" name="description"/><br>
+    <input type="submit"/>
+</form>
 <?php foreach ($res as $value): ?>
     <div class="Box">
         <p><span><?php echo $value['name']; ?></span></p>
