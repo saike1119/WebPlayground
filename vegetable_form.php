35ad791cf2f39de5e6a51970a63de75b111662b6
saike1119 <saike1119@gmail.com>
2018-02-25 00:32:01 +0900

    とりあえず画像をh1に挿入

--- vegetable_form.php
+++ vegetable_form.php
@@ -28,7 +28,7 @@ $mysqli->close();
     <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 <body>
-<h1>お野菜管理</h1>
+<h1>お野菜管理<img src="./img/vegetable_main.png" alt="logo" width="5%" height="5%"></h1>
 <form action="insert_vegetable.php" method="post">
     野菜名: <input title="名前を入力" type="text" name="name"/><br>
     価格: <input title="価格を入力" type="text" name="price"/><br>
