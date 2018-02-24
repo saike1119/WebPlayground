1ceb92bcbae6fc547fc1d90e54f354ff650e9aec
saike1119 <saike1119@gmail.com>
2018-02-25 00:21:27 +0900

    ボタン名変更

--- vegetable_form.php
+++ vegetable_form.php
@@ -33,7 +33,7 @@ $mysqli->close();
     野菜名: <input title="名前を入力" type="text" name="name"/><br>
     価格: <input title="価格を入力" type="text" name="price"/><br>
     概要: <input title="概要を入力" type="text" name="description"/><br>
-    <input type="submit"/>
+    <input type="submit" value="野菜を追加する"/>
 </form>
 <?php foreach ($res as $value): ?>
     <div class="ContentBox">
