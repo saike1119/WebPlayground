1c79109837cd7e623b1247c5105ce16b50d25d1f
saike1119 <saike1119@gmail.com>
2018-02-27 05:34:15 +0900

    href修正

--- vegetable_add.php
+++ vegetable_add.php
@@ -10,13 +10,13 @@
 <h1>お野菜管理<img src="./img/vegetable_main.png" alt="logo" width="50" height="50"></h1>
 <ul class="globalNav">
     <li class="active">
-        <a href="#">
+        <a href="http://192.168.33.10/vegetable_home.php">
             <strong>ホーム</strong>
             <span>home</span>
         </a>
     </li>
     <li>
-        <a href="#">
+        <a href="http://192.168.33.10/vegetable_add.php">
             <strong>お野菜追加</strong>
             <span>add</span>
         </a>
