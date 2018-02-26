b4828dad98bc61feab37e896e97329357b96eab8
saike1119 <saike1119@gmail.com>
2018-02-27 05:31:59 +0900

    vegetable_add.php追加

---
+++ vegetable_add.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>form</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<h1>お野菜管理<img src="./img/vegetable_main.png" alt="logo" width="50" height="50"></h1>
+<ul class="globalNav">
+    <li class="active">
+        <a href="#">
+            <strong>ホーム</strong>
+            <span>home</span>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <strong>お野菜追加</strong>
+            <span>add</span>
+        </a>
+    </li>
+    <li>
+        <a href="#">
+            <strong>お野菜削除</strong>
+            <span>delete</span>
+        </a>
+    </li>
+</ul>
+<form action="insert_vegetable.php" method="post">
+    野菜名: <input title="名前を入力" type="text" name="name"/><br>
+    価格: <input title="価格を入力" type="text" name="price"/><br>
+    概要: <input title="概要を入力" type="text" name="description"/><br>
+    <input type="submit" value="野菜を追加する"/>
+</form>
+</body>
+</html>
