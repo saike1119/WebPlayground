af7b49f47053f4c7bf44c90f82743dc9b6d98676
saike1119 <saike1119@gmail.com>
2018-02-28 01:42:28 +0900

    div追加

--- vegetable_home.php
+++ vegetable_home.php
@@ -28,7 +28,9 @@ $mysqli->close();
     <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 <body>
+<div class="header">
 <h1>お野菜管理<img src="./img/vegetable_main.png" alt="logo" width="50" height="50"></h1>
+</div>
 <ul class="globalNav">
     <li class="active">
         <a href="http://192.168.33.10/vegetable_home.php">
