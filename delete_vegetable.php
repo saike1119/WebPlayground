9bb91bd9d9579ac1ca88ec34a8d00f12ea538c8f
saike1119 <saike1119@gmail.com>
2018-03-02 00:14:04 +0900

    delete_vegetable.php追加

---
+++ delete_vegetable.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>form</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<?php
+
+include_once('db_def.php');
+
+$mysqli = new mysqli($dbhost, $dbuser, $dbpasswd, $dbname);
+
+if ($mysqli->connect_error) {
+    echo $mysqli->connect_errno . ' : ' . $mysqli->connect_error;
+    exit();
+}
+
+$mysqli->set_charset('utf8');
+
+$mysqli->close();
+
+?>
+<br>
+<input type="button" value="前のページへ戻る" onClick="location.href='vegetable_home.php'">
+</body>
+</html>
