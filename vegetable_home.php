13b179db2f0ad45727b73d0b2f77add63d015cea
saike1119 <saike1119@gmail.com>
2018-03-02 01:10:55 +0900

    TODO追加

--- vegetable_home.php
+++ vegetable_home.php
@@ -40,6 +40,10 @@ $mysqli->close();
     <li>
         <a href="http://192.168.33.10/vegetable_delete.php">お野菜削除</a>
     </li>
+    <li>
+        <!--TODO-->
+        <a href="#">お野菜更新</a>
+    </li>
 </ul>
 <?php foreach ($res as $value): ?>
     <div class="contentBox">
