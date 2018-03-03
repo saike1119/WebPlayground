c1431aa96a47b5a4414f409e828ad0326ae811c3
saike1119 <saike1119@gmail.com>
2018-03-03 21:10:01 +0900

    vvegetable_update.php追加

---
+++ vegetable_update.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>form</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<div class="header">
+    <h1>お野菜管理<img src="./img/vegetable_main.png" alt="logo" width="50" height="50"></h1>
+</div>
+<ul id="nav">
+    <li>
+        <a href="http://192.168.33.10/vegetable_home.php">ホーム</a>
+    </li>
+    <li>
+        <a href="http://192.168.33.10/vegetable_add.php">お野菜追加</a>
+    </li>
+    <li>
+        <a href="http://192.168.33.10/vegetable_delete.php">お野菜削除</a>
+    </li>
+</ul>
+<p>・更新する野菜の管理IDを入力してください</p>
+<form action="delete_vegetable.php" method="post">
+    管理ID: <input title="管理IDを入力" type="text" name="id"/><br>
+    <input type="submit" value="野菜を更新する"/>
+</body>
+</html>
