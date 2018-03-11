35dd432b835101d816f0c3d7f24b155ae0602ab9
saike1119 <saike1119@gmail.com>
2018-03-11 17:00:28 +0900

    GPAのフォームhtmlを作成

--- GPAapp/GPA_app.php
+++ GPAapp/GPA_app.php
@@ -1,1 +1,24 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>GPA計算アプリ(文教大学対応)</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+<h1>GPA計算アプリ(文教大学対応)</h1>
+<p>GPAを計算します。AA,A,B,C,D,F,単位数をそれぞれ入力してください。</p>
+<p>なお、入力は全て半角英数字で入力してください。</p>
+<form action="#" method="post">
+    AA: <input title="AAを入力" type="text" name="AA"/><br>
+    A: <input title="Aを入力" type="text" name="A"/><br>
+    B: <input title="Bを入力" type="text" name="B"/><br>
+    C: <input title="Cを入力" type="text" name="C"/><br>
+    D: <input title="Dを入力" type="text" name="D"/><br>
+    F: <input title="Fを入力" type="text" name="F"/><br><br>
+    取得単位数: <input title="取得単位数を入力" type="text" name="unit"/><br>
+    <input type="submit" value="GPAを計算する"/>
+</form>
+</body>
+</html>
