b59c1b318b0cf9d9882fc95e6e912cd55f443ce9
saike1119 <saike1119@gmail.com>
2018-03-14 01:36:04 +0900

    GPA計算処理追加

--- GPAapp/GPA_calc.php
+++ GPAapp/GPA_calc.php
@@ -1,8 +1,25 @@
 <?php
 
-class GPA_calc
-{
+$AA = $_POST['AA'] * 4 * 2;
+$A = $_POST['A'] * 3 * 2;;
+$B = $_POST['B'] * 2 * 2;
+$C = $_POST['C'] * 2;
+$D = $_POST['D'];
+$F = $_POST['F'];
+$unit = $_POST['unit'];
 
+if ($D >= 0) {
+    $D = 0;
+}
+if ($F >= 0) {
+    $F = 0;
 }
 
-$const = new GPA_calc();
+$sumPoints = $AA + $A + $B + $C + $D + $F;
+$GPA = $sumPoints / $unit;
+
+if ($GPA >= 3.5) {
+    echo 'あなたのGPAは' . $GPA . 'です。優秀です！';
+} else {
+    echo 'あなたのGPAは' . $GPA . 'です。普通です。';
+}
