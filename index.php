9fc1569f5f2d7ba66d02cb5fd996eca9bb1a134e
saike1119 <saike1119@gmail.com>
2018-03-17 14:15:16 +0900

    概要とヘッダー追加

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
     <link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 <body>
-<h1>ようこそ！WebPlaygroundへ！</h1>
+<div class="header">
+    <h1>ようこそ！WebPlaygroundへ！</h1>
+</div>
 <div class="textBox"></div>
 <p>ここでは様々なWebに関する技術を楽しめます</p>
 <h3>PlayGround</h3>
 <ul type="disc">
-    <li><a href="walk_form.php">歩数計算</a><br></li>
+    <li><a href="walk_form.php">歩数計算</a><br>
+        <p>概要:歩数を計算できるアプリです。</p></li>
     <li><a href="vegetable_home.php">お野菜管理</a></li>
     <li><a href="#">メンバーシャッフル</a></li>
     <li><a href="/GPAapp/GPA_app.php">GPA計算</a><br></li>
