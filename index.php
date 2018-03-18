3f1a0d6455de988f6b789a20b702cb2416e36c8e
saike1119 <saike1119@gmail.com>
2018-03-18 14:16:55 +0900

    概要追加

--- index.php
+++ index.php
@@ -10,15 +10,19 @@
     <h1>ようこそ！WebPlaygroundへ！</h1>
 </div>
 <div class="textBox"></div>
-<p>ここでは様々なWebに関する技術を楽しめます</p>
+<p>ここでは様々なWebに関する技術を楽しむことができます。絶賛暇な時弄り中。</p>
 <h3>PlayGround</h3>
 <ul type="disc">
     <li><a href="walk_form.php">歩数計算</a><br>
         <p>概要:歩数を計算できるアプリです。</p></li>
-    <li><a href="vegetable_home.php">お野菜管理</a></li>
-    <li><a href="#">メンバーシャッフル</a></li>
-    <li><a href="/GPAapp/GPA_app.php">GPA計算</a><br></li>
-    <li><a href="/TODO/TODO_app.php">TODO管理</a><br></li>
+    <li><a href="vegetable_home.php">お野菜管理</a><br>
+        <p>概要:歩数を計算できるアプリです。</p></li>
+    <li><a href="#">メンバーシャッフル</a><br>
+        <p>概要:歩数を計算できるアプリです。</p></li>
+    <li><a href="/GPAapp/GPA_app.php">GPA計算</a><br>
+        <p>概要:歩数を計算できるアプリです。</p></li>
+    <li><a href="/TODO/TODO_app.php">TODO管理</a><br>
+        <p>概要:歩数を計算できるアプリです。</p></li>
 </ul>
 </body>
 </html>
