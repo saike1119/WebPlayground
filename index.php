f9f83a2459fee2474b155051953b21adbe62e7e3
saike1119 <saike1119@gmail.com>
2018-03-19 17:57:36 +0900

    リンク追加

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <p>概要:簡単な歩数計算ができます。算数レベルです。</p></li>
     <li><a href="vegetable_home.php">お野菜管理</a><br>
         <p>概要:お野菜の管理ができます。閲覧から登録、削除、更新までの機能があります。</p></li>
-    <li><a href="#">メンバーシャッフル</a><br>
+    <li><a href="member_shuffle_home.php">メンバーシャッフル</a><br>
         <p>~絶賛開発中~</p></li>
     <li><a href="/GPA/GPA_app.php">GPA計算</a><br>
         <p>概要:GPAの計算ができます。最高評価4のものに対応しています。</p></li>
