6fd57b5ff6e3376cdcde5928cb2fa36c010a8315
saike1119 <saike1119@gmail.com>
2018-04-03 00:47:16 +0900

    htmlにパーティクル処理を追加

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <title>WebPlayground</title>
     <link rel="stylesheet" type="text/css" href="./css/style.css">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type='text/javascript' src='js/jquery.particleground.js'></script>
+    <script type='text/javascript' src='js/demo.js'></script>
     <script>
         $(function () {
             var topBtn = $('#pageTop');
@@ -30,24 +32,27 @@
     <h1>ようこそ！WebPlaygroundへ！</h1>
 </div>
 <p id="pageTop"><a href="#">🔝</a></p>
-<div class="textBox"></div>
-<p>ここでは様々なWebに関する技術を楽しむことができます。絶賛暇な時いじり中。</p>
-<h3>🔥PlayGroundList🔥</h3>
-<ul type="disc">
-    <li><a href="Walk/walk_form.php">歩数計算</a><br>
-        <p>概要:簡単な歩数計算ができます。算数レベルです。</p></li>
-    <li><a href="Vegetable/vegetable_home.php">お野菜管理</a><br>
-        <p>概要:お野菜の管理ができます。閲覧から登録、削除、更新までの機能があります。</p></li>
-    <li><a href="member/member_shuffle_home.php">メンバーシャッフル</a><br>
-        <p>概要:プレゼン発表の順番をワンクリックで決めることができます。また、１日ごとにメンバーが強制シャッフルされます。</p></li>
-    <li><a href="/GPA/GPA_app.php">GPA計算</a><br>
-        <p>概要:GPAの計算ができます。最高評価4のものに対応しています。</p></li>
-    <li><a href="/login/Main.php">会員情報管理</a><br>
-        <p>概要:会員情報管理を実装しました。</p></li>
-    <li><a href="/TODO/TODO_app.php">TODO管理</a><br>
-        <p>概要:簡単なタスク管理ができるTODOアプリです。</p></li>
-    <li><a href="#">Yahoo!API ショッピング編</a><br>
-        <p>絶賛開発中</p></li>
-</ul>
+<div id="particles">
+    <div id="intro">
+        <p>ここでは様々なWebに関する技術を楽しむことができます。絶賛暇な時いじり中。</p>
+        <h3>🔥PlayGroundList🔥</h3>
+        <ul>
+            <li><a href="Walk/walk_form.php">歩数計算</a><br>
+                <p>概要:簡単な歩数計算ができます。算数レベルです。</p></li>
+            <li><a href="Vegetable/vegetable_home.php">お野菜管理</a><br>
+                <p>概要:お野菜の管理ができます。閲覧から登録、削除、更新までの機能があります。</p></li>
+            <li><a href="member/member_shuffle_home.php">メンバーシャッフル</a><br>
+                <p>概要:プレゼン発表の順番をワンクリックで決めることができます。また、１日ごとにメンバーが強制シャッフルされます。</p></li>
+            <li><a href="/GPA/GPA_app.php">GPA計算</a><br>
+                <p>概要:GPAの計算ができます。最高評価4のものに対応しています。</p></li>
+            <li><a href="/login/Main.php">会員情報管理</a><br>
+                <p>概要:会員情報管理を実装しました。</p></li>
+            <li><a href="/TODO/TODO_app.php">TODO管理</a><br>
+                <p>概要:簡単なタスク管理ができるTODOアプリです。</p></li>
+            <li><a href="#">Yahoo!API ショッピング編</a><br>
+                <p>絶賛開発中</p></li>
+        </ul>
+    </div>
+</div>
 </body>
 </html>
